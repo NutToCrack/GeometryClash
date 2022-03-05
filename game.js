c80525c536da066a524fe7b30c8730839e2f94f8
Karol Orzechowski <karol.orzechowski1337@gmail.com>
2022-03-05 13:19:46 +0100

    chuj

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+import player from "./player";
+
 const gameObjects = [];
 const canvasSize = {
     width: 500,
@@ -5,24 +7,13 @@ const canvasSize = {
 }
 const canvasManager = {
     lastRefresh: Date.now(),
-    currentCanvas: document.getElementById('canvas1'),
-    bufferedCanvas: document.getElementById('canvas2'),
-    repaint() {
-        const ctx = this.bufferedCanvas.getContext('2d');
-        const deltaTime = Date.now() - this.lastRefresh;
+    refresh() {
+        const deltaTime = (Date.now() - this.lastRefresh) / 50;
         this.lastRefresh = Date.now();
-        ctx.clearRect(0, 0, canvasSize.width, canvasSize.height);
-        gameObjects.forEach(go => go.draw(ctx, deltaTime));
-        this.currentCanvas.style.visibility = 'hidden';
-        this.bufferedCanvas.style.visibility = 'visible';
-        [this.bufferedCanvas, this.currentCanvas] = [this.currentCanvas, this.bufferedCanvas];
+        gameObjects.forEach(go => go.draw(deltaTime));
     },
 }
 
-const start = Date.now();
+gameObjects.push(player);
 
-gameObjects.push({draw(ctx) {
-    ctx.fillRect((Date.now() - start), 0, 100, 100);
-}});
-
-setInterval(() => canvasManager.repaint(), 33);
+setInterval(() => canvasManager.refresh(), 33);
--- player.js
+++ player.js
@@ -1,9 +1,18 @@
-export default const player = {
+const playerElem = document.createElement('canvas');
+playerElem.setAttribute('width', 40);
+playerElem.setAttribute('height', 40);
+playerElem.getContext('2d').fillRect(0, 0, 40, 40);
+
+const player = {
+    elem: playerElem,
     position: {x: 0, y: 0},
-    velocity: {x: 0, y: 0},
-    draw(ctx, deltaTime) {
+    velocity: {x: 1, y: 1},
+    draw(deltaTime) {
         this.x += this.velocity.x * deltaTime
         this.y += this.velocity.y * deltaTime
-        ctx.
+        this.elem.style.left = x;
+        this.elem.style.top = y;
     }
 }
+
+export default player;
