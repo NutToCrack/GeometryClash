d0a36251f1bf1a2394aafc44368d0f20d7342824
Jeremi Sobierski <tbuli1112@gmail.com>
2022-03-05 13:59:17 +0100

    one file

--- game.js
+++ game.js
@@ -1,10 +1,30 @@
-import player from "./player";
+const player = {
+    elem: function() {
+        const playerElem = document.createElement('canvas');
+        playerElem.setAttribute('width', 40);
+        playerElem.setAttribute('height', 40);
+        playerElem.getContext('2d').fillRect(0, 0, 40, 40);
+        document.querySelector('body').appendChild(playerElem);
+        return playerElem;
+    }(),
+    position: {x: 0, y: 0},
+    velocity: {x: 0, y: 0},
+    draw(deltaTime) {
+        this.position.x += this.velocity.x * deltaTime
+        this.position.y += this.velocity.y * deltaTime
+        
+        this.elem.style.left = this.position.x;
+        this.elem.style.top = this.position.y;
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    switch (event.key) {
+        case ''
+    }
+})
 
 const gameObjects = [];
-const canvasSize = {
-    width: 500,
-    height: 500
-}
 const canvasManager = {
     lastRefresh: Date.now(),
     refresh() {
--- player.js
+++ player.js
@@ -1,17 +1,19 @@
-const playerElem = document.createElement('canvas');
-playerElem.setAttribute('width', 40);
-playerElem.setAttribute('height', 40);
-playerElem.getContext('2d').fillRect(0, 0, 40, 40);
-
 const player = {
-    elem: playerElem,
+    elem: function() {
+        const playerElem = document.createElement('canvas');
+        playerElem.setAttribute('width', 40);
+        playerElem.setAttribute('height', 40);
+        playerElem.getContext('2d').fillRect(0, 0, 40, 40);
+        document.querySelector('body').appendChild(playerElem);
+        return playerElem;
+    }(),
     position: {x: 0, y: 0},
     velocity: {x: 1, y: 1},
     draw(deltaTime) {
         this.x += this.velocity.x * deltaTime
         this.y += this.velocity.y * deltaTime
-        this.elem.style.left = x;
-        this.elem.style.top = y;
+        this.elem.style.left = this.x;
+        this.elem.style.top = this.y;
     }
 }
 
